79d173a92d44945e8526c362e792055383ebaebb
slawek <slawomir.zytko@gmail.com>
2014-04-29 15:31:49 +0200

    Changed service class name;

--- app/config/services.php
+++ app/config/services.php
@@ -1,7 +1,7 @@
 <?php return array (
   'MongoServiceProvider' => '/var/www/vegas-cmf/vegas-demo/app/services/MongoServiceProvider.php',
   'CollectionManagerServiceProvider' => '/var/www/vegas-cmf/vegas-demo/app/services/CollectionManagerServiceProvider.php',
-  'ViewCacheServiceManager' => '/var/www/vegas-cmf/vegas-demo/app/services/ViewCacheServiceManager.php',
+  'ViewCacheServiceProvider' => '/var/www/vegas-cmf/vegas-demo/app/services/ViewCacheServiceProvider.php',
   'UrlServiceProvider' => '/var/www/vegas-cmf/vegas-demo/app/services/UrlServiceProvider.php',
   'AssetsServiceProvider' => '/var/www/vegas-cmf/vegas-demo/app/services/AssetsServiceProvider.php',
 );
